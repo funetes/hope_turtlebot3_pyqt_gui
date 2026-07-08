8745bed35b377d491a9f6bcd0b5abfea9fa9ef91
hwan <kimlove2324@gmail.com>
2026-07-08 10:30:46 +0900

    try문 삭제

--- src/turtlebot3_pyqt_gui/setup.py
+++ src/turtlebot3_pyqt_gui/setup.py
@@ -13,10 +13,7 @@ setup(
         'signals',
         "turtlebot3_pyqt_gui.ros",
     ],
-    package_dir={
-        'turtlebot3_pyqt_gui': 'turtlebot3_pyqt_gui',
-        "signals": "../signals"
-        },
+    package_dir={'turtlebot3_pyqt_ui': 'turtlebot3_pyqt_gui'},
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
