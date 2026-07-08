239bb8d1b2b7d09bb731043fe447f06283edc169
changmin <changminc@gmail.com>
2026-07-08 18:09:02 +0900

    yaml 파일을 파일 탐색기를 통해 선택 가능하도록 고도화

--- src/turtlebot3_pyqt_gui/turtlebot3_pyqt_gui/viewmodels/waypoint_viewmodel.py
+++ src/turtlebot3_pyqt_gui/turtlebot3_pyqt_gui/viewmodels/waypoint_viewmodel.py
@@ -1,22 +1,58 @@
 from PyQt5.QtCore import QObject, pyqtSignal, pyqtSlot
 
+from ..services.waypoints_yaml_load_service import WaypointYamlLoadService
+from ..models.waypoint import Waypoint
 
 class WaypointViewModel(QObject):
-    waypoint_added = pyqtSignal(str)
+
+    waypoints_loaded = pyqtSignal(list)
+    waypoint_selected = pyqtSignal(Waypoint)
     waypoint_requested = pyqtSignal(str)
 
     def __init__(self):
         super().__init__()
-        self.current_waypoint = ""
+        self._yaml_service = WaypointYamlLoadService()
+        self._waypoints: list[Waypoint] = []
 
-    @pyqtSlot(float, float, float)
-    def add_waypoint(self, x, y, yaw):
-        self.current_waypoint = f"X:{x:.2f}, Y:{y:.2f}, Yaw:{yaw:.2f}"
-        self.waypoint_added.emit(self.current_waypoint)
+        self._selected_waypoint: Waypoint | None  = None
+
+    @property
+    def selected_waypoint(self):
+        return self._selected_waypoint
+
+    def load_waypoints(self, path):
+
+        self._waypoints = self._yaml_service.load(path)
+
+        print(self._waypoints)
+
+        self.waypoints_loaded.emit(
+            [wp.name for wp in self._waypoints]
+        )
+
+        if self._waypoints:
+            self.select_waypoint(0)
+
+
+    # @pyqtSlot(float, float, float)
+    # def add_waypoint(self, x, y, yaw):
+        # self.current_waypoint = f"X:{x:.2f}, Y:{y:.2f}, Yaw:{yaw:.2f}"
+        # self.waypoint_added.emit(self.current_waypoint)
 
     @pyqtSlot()
     def go_to_waypoint(self):
-        if not self.current_waypoint:
-            self.waypoint_requested.emit("No waypoint set")
+        pass
+
+
+    @pyqtSlot(int)
+    def select_waypoint(self, index: int):
+
+        if index < 0:
             return
-        self.waypoint_requested.emit(f"Moving to {self.current_waypoint}")
+
+        if index >= len(self._waypoints):
+            return
+
+        self._selected_waypoint = self._waypoints[index]
+
+        self.waypoint_selected.emit(self._selected_waypoint)
