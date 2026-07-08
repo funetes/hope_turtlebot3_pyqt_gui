3188cd7dcd5350d1cf9205c879cff9dbffa7570a
changmin <changminc@gmail.com>
2026-07-08 11:26:48 +0900

    배터리 상태 구독 추가

--- src/turtlebot3_pyqt_gui/turtlebot3_pyqt_gui/ros/turtlebot3_pyqt_gui_node.py
+++ src/turtlebot3_pyqt_gui/turtlebot3_pyqt_gui/ros/turtlebot3_pyqt_gui_node.py
@@ -1,12 +1,23 @@
 import rclpy
 from rclpy.node import Node
 
+from signals.RosSignalsManager import SignalsManager
+from sensor_msgs.msg import BatteryState
+
 
 class Turtlebot3PyQtGuiNode(Node):
     def __init__(self):
         super().__init__("turtlebot3_pyqt_gui")
         self._timer = self.create_timer(0.01, self._spin_once)
 
+        self.signalsManager = SignalsManager
+
+        self.battery_sub = self.create_subscription(
+            BatteryState, "/battery_state", self.battery_state_callback, 10
+        )
+
+    def battery_state_callback(self, msg: BatteryState):
+        self.signalsManager.battery_status_received.emit(msg)
 
     def _spin_once(self):
         rclpy.spin_once(self, timeout_sec=0)
