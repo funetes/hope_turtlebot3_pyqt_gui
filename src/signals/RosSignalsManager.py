3029d72a565cf220e05ecd6af9ed99a1f19ebfaa
changmin <changminc@gmail.com>
2026-07-08 14:36:13 +0900

    ros2 topic monitor 영역에 표시할 데이터 구독 추가

--- src/signals/RosSignalsManager.py
+++ src/signals/RosSignalsManager.py
@@ -2,13 +2,13 @@ from PyQt5.QtCore import QObject, pyqtSignal
 
 from sensor_msgs.msg import BatteryState
 
-
 class ROSSignalsManager(QObject):
 
     # 필요한 시그널 정의
     service_response_received = pyqtSignal(dict)
     node_status_changed = pyqtSignal(str)
     battery_status_received = pyqtSignal(BatteryState)
+    robot_topic_info_received = pyqtSignal(object)
 
     def __init__(self):
       super().__init__()
@@ -21,6 +21,10 @@ class ROSSignalsManager(QObject):
 
     def emit_battery_status(self, status: BatteryState):
         self.battery_status_received.emit(status)
+
+
+    def emit_robot_topic_info(self, robot_topic_info):
+        self.robot_topic_info_received.emit(robot_topic_info)
 
 
 # 파일의 맨 아래에서 인스턴스를 미리 '하나만' 생성해 둡니다.
--- src/turtlebot3_pyqt_gui/turtlebot3_pyqt_gui/ros/turtlebot3_pyqt_gui_node.py
+++ src/turtlebot3_pyqt_gui/turtlebot3_pyqt_gui/ros/turtlebot3_pyqt_gui_node.py
@@ -1,23 +1,76 @@
 import rclpy
 from rclpy.node import Node
 
+from nav_msgs.msg import Odometry
 from signals.RosSignalsManager import SignalsManager
-from sensor_msgs.msg import BatteryState
+from sensor_msgs.msg import BatteryState, LaserScan
+from tf_transformations import euler_from_quaternion
+import math
+from dataclasses import dataclass
 
+
+@dataclass(slots=True)
+class RobotTopicInfo:
+    pos_x: float = 0.0
+    pos_y: float = 0.0
+    yaw: float = 0.0
+    min_distance: float = float("inf")
 
 class Turtlebot3PyQtGuiNode(Node):
     def __init__(self):
         super().__init__("turtlebot3_pyqt_gui")
-        self._timer = self.create_timer(0.01, self._spin_once)
+        # self._timer = self.create_timer(0.01, self._spin_once)
 
         self.signalsManager = SignalsManager
 
+        self.robot_topic_info = RobotTopicInfo()
+
+        self._robot_topic_timer = self.create_timer(0.5, self._robot_topic_emit)
+
+        self.set_subscription()
+
+    def set_subscription(self):
+
         self.battery_sub = self.create_subscription(
-            BatteryState, "/battery_state", self.battery_state_callback, 10
+            BatteryState, "/battery_state", self._battery_state_callback, 10
         )
 
-    def battery_state_callback(self, msg: BatteryState):
+        self.odom_sub = self.create_subscription(
+            Odometry,
+            "/odom",
+            self._odom_callback,
+            10,
+        )
+
+        self.scan_sub = self.create_subscription(
+            LaserScan,
+            "/scan",
+            self._scan_callback,
+            10,
+        )
+
+
+    def _odom_callback(self, msg: Odometry):
+
+        self.robot_topic_info.pos_x = msg.pose.pose.position.x
+        self.robot_topic_info.pos_y = msg.pose.pose.position.y
+        q = msg.pose.pose.orientation
+        _, _, self.robot_topic_info.yaw = euler_from_quaternion([q.x, q.y, q.z, q.w])
+
+    def _scan_callback(self, msg: LaserScan):
+        valid = [r for r in msg.ranges if not math.isinf(r) and not math.isnan(r)]
+
+        self.robot_topic_info.min_distance = min(valid) if valid else float("inf")
+
+
+    def _battery_state_callback(self, msg: BatteryState):
         self.signalsManager.battery_status_received.emit(msg)
 
-    def _spin_once(self):
-        rclpy.spin_once(self, timeout_sec=0)
+    def _robot_topic_emit(self):
+
+        self.get_logger().info("_robot_topic_emit")
+        print(self.robot_topic_info)
+        self.signalsManager.robot_topic_info_received.emit(self.robot_topic_info)
+
+    # def _spin_once(self):
+    #     rclpy.spin_once(self, timeout_sec=0)
