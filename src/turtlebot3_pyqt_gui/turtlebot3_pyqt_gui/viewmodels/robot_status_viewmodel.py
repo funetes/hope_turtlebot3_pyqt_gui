3252ad57534d33c4775993178aa31f07e638c1fb
hwan <kimlove2324@gmail.com>
2026-07-08 10:33:19 +0900

    min_scan, last_cmd 삭제

--- src/turtlebot3_pyqt_gui/turtlebot3_pyqt_gui/viewmodels/robot_status_viewmodel.py
+++ src/turtlebot3_pyqt_gui/turtlebot3_pyqt_gui/viewmodels/robot_status_viewmodel.py
@@ -1,3 +1,4 @@
+import os
 from PyQt5.QtCore import QObject, pyqtSignal, pyqtSlot
 
 
@@ -13,8 +14,10 @@ class RobotStatusViewModel(QObject):
 
     @pyqtSlot()
     def connect(self):
-        self.ros_state = "Connected"
-        self.min_scan = "0.25 m"
+        domain_id = '60'
+        os.environ['ROS_DOMAIN_ID'] = domain_id if domain_id else '60'
+        self.ros_state = f'domain_id: {domain_id} connected.'
+        self.min_scan = ""
         self._emit_status()
 
     @pyqtSlot()
@@ -25,11 +28,6 @@ class RobotStatusViewModel(QObject):
     @pyqtSlot()
     def exit(self):
         self._emit_status("Application exit requested")
-
-    @pyqtSlot(float, float)
-    def update_cmd_vel(self, linear, angular):
-        self.last_cmd = f"linear: {linear:.2f}, angular: {angular:.2f}"
-        self._emit_status()
 
     def _emit_status(self, message=None):
         if message is None:
