70a8375aad0af8186b3d7286f44e1794efba7ea5
hwan <kimlove2324@gmail.com>
2026-07-09 14:59:23 +0900

    log_viewmodel에서 signalManager사용

--- src/turtlebot3_pyqt_gui/turtlebot3_pyqt_gui/viewmodels/log_viewmodel.py
+++ src/turtlebot3_pyqt_gui/turtlebot3_pyqt_gui/viewmodels/log_viewmodel.py
@@ -1,11 +1,15 @@
 from PyQt5.QtCore import QObject, pyqtSignal
-
+from ..signals.RosSignalsManager import SignalsManager
 
 class LogViewModel(QObject):
     log_updated = pyqtSignal(str)
 
     def __init__(self):
         super().__init__()
+        self.signal_manager = SignalsManager
+        self.signal_manager.log_msg_rceived.connect(self.append_log)
 
     def append_log(self, message):
         self.log_updated.emit(message)
+
+    
